b66b96a4437dffe15b2f22f57f5497710367aaa4
Guo Yunhe <i@guoyunhe.me>
2025-01-10 23:19:03 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-export interface ResizeObserverPolyfillOptions {
-  lastNameUpperCase?: boolean;
-}
-
-export function resizeObserverPolyfill(firstName: string, lastName: string, options?: ResizeObserverPolyfillOptions) {
-  if (options?.lastNameUpperCase) {
-    return firstName + ' ' + lastName.toLocaleUpperCase();
-  }
-  return firstName + ' ' + lastName;
-}
+export default ResizeObserver
